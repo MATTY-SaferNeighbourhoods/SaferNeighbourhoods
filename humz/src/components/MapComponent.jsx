85cd96ef93db7ec78a8d66c4bfef0c439203d7ff
Taro yamashita <taro.c.yamashita@gmail.com>
2018-11-24 14:45:31 -0500

    added map component

--- humz/src/components/MapComponent.jsx
+++ humz/src/components/MapComponent.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import GoogleMapReact from 'google-map-react';
+require('dotenv').config();
+
+
+
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 
 class MapComponent extends Component {
     constructor(props){
@@ -14,13 +22,14 @@ class MapComponent extends Component {
 
     componentDidMount(){
         //Theft Data; 
+        console.log(process.env);
         axios.get('https://api.namara.io/v0/data_sets/38ec612b-f522-418f-95f8-10c0fdc8bd52/data/en-0?geometry_format=wkt&api_key=73b3a4ac67e7ec0cfc3c69a958f29487d1fb0684fc2db7b2b533b8df7922cd3c&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c&limit=10').then((data)=>{
             console.log("theft data", data.data);
             this.setState({theftData:data.data});
         })
         //Assault Data;
         axios.get('https://api.namara.io/v0/data_sets/e544c601-4ea3-4a56-8130-1d062d4f29ab/data/en-0?geometry_format=wkt&api_key=73b3a4ac67e7ec0cfc3c69a958f29487d1fb0684fc2db7b2b533b8df7922cd3c&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c&limit=10').then((data)=>{
-            console.log('Assuault Data', data.data);
+            console.log('Assault Data', data.data);
             this.setState({theftData:data.data})
         })
         //Fatal Collisions
@@ -32,6 +41,20 @@ class MapComponent extends Component {
         return (
             <div>
                 <h1> Map Goes Here </h1>
+                <div style={{ height: '50vh', width: '100%' }}>
+                <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyAhxD_46rmSKa-1LlFjGO4-eY2KuyWmzTY'}}
+                defaultCenter={{lat:43.802116, 
+                                lng: -79.17601}}
+                defaultZoom={9}
+                >
+                <AnyReactComponent
+                    lat={43.802116}
+                    lng={-79.17601}
+                    text={'Taro Yamashita'}
+                />
+                </GoogleMapReact>
+                </div>
             </div>
         )
     }
