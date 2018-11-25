0fe56765a3951a6cd09dcdaa251f0a23c7d5f4de
Taro yamashita <taro.c.yamashita@gmail.com>
2018-11-25 12:56:46 -0500

    update results

--- humz/src/pages/Discussions/MapComponent.jsx
+++ humz/src/pages/Discussions/MapComponent.jsx
@@ -4,6 +4,7 @@ import GoogleMapReact from "google-map-react";
 import AssaultComponent from "./AssaultComponent.jsx";
 import TheftComponent from "./TheftComponent.jsx";
 import CollisionComponent from "./CollisionComponent.jsx";
+import { runInThisContext } from "vm";
 require("dotenv").config();
 
 class MapComponent extends Component {
@@ -16,19 +17,21 @@ class MapComponent extends Component {
       theftData: [],
       assaultData: [],
       collisionData: [],
-      limit: 20
+      limit: 250,
+      shownLimit: 10
     };
     this.renderAssaults = this.renderAssaults.bind(this);
     this.renderThefts = this.renderThefts.bind(this);
     this.renderCollisions = this.renderCollisions.bind(this);
     this.getLocation = this.getLocation.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
   }
 
   componentDidMount() {
     //Theft Data;
     axios
       .get(
-        `https://api.namara.io/v0/data_sets/38ec612b-f522-418f-95f8-10c0fdc8bd52/data/en-0?geometry_format=wkt&api_key=73b3a4ac67e7ec0cfc3c69a958f29487d1fb0684fc2db7b2b533b8df7922cd3c&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c/export&limit=${
+        `https://api.namara.io/v0/data_sets/38ec612b-f522-418f-95f8-10c0fdc8bd52/data/en-0?geometry_format=wkt&api_key=e7fec4e08b35a6956788ad970636d495e7478065d3ec5663cf4776688956d7ed&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c/export&limit=${
           this.state.limit
         }&order=occurrencedate DESC`
       )
@@ -38,7 +41,7 @@ class MapComponent extends Component {
     //Assault Data;
     axios
       .get(
-        `https://api.namara.io/v0/data_sets/e544c601-4ea3-4a56-8130-1d062d4f29ab/data/en-0?geometry_format=wkt&api_key=73b3a4ac67e7ec0cfc3c69a958f29487d1fb0684fc2db7b2b533b8df7922cd3c&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c&limit=${
+        `https://api.namara.io/v0/data_sets/e544c601-4ea3-4a56-8130-1d062d4f29ab/data/en-0?geometry_format=wkt&api_key=e7fec4e08b35a6956788ad970636d495e7478065d3ec5663cf4776688956d7ed&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c&limit=${
           this.state.limit
         }&order=occurrencedate DESC`
       )
@@ -48,7 +51,7 @@ class MapComponent extends Component {
     //Fatal Collisions
     axios
       .get(
-        `https://api.namara.io/v0/data_sets/74287125-2cc6-4da1-8079-d59a09acd187/data/en-0?geometry_format=wkt&api_key=73b3a4ac67e7ec0cfc3c69a958f29487d1fb0684fc2db7b2b533b8df7922cd3c&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c&order=date DESC&limit=${
+        `https://api.namara.io/v0/data_sets/74287125-2cc6-4da1-8079-d59a09acd187/data/en-0?geometry_format=wkt&api_key=e7fec4e08b35a6956788ad970636d495e7478065d3ec5663cf4776688956d7ed&organization_id=5bdc5265c0b35c45d030159b&project_id=5bdc5ca1b0006245734c433c&order=date DESC&limit=${
           this.state.limit
         }`
       )
@@ -74,12 +77,23 @@ class MapComponent extends Component {
     // geolocation
   };
 
+  changeHandler = e => {
+    this.setState({ shownLimit: e.target.value }, () => {});
+    console.log("the limit does not exist!", this.state.shownLimit);
+  };
+
   render() {
     return (
       <div>
-        <h1> Map Goes Here </h1>
+        <h1> Discussion Board </h1>
 
         <div style={{ height: "50vh", width: "100%" }}>
+          <form>
+            <input
+              value={this.state.shownLimit}
+              onChange={this.changeHandler}
+            />
+          </form>
           <button onClick={() => this.renderCollisions()}>
             {" "}
             Show Collisions{" "}
@@ -94,29 +108,41 @@ class MapComponent extends Component {
             defaultZoom={11}
           >
             {this.state.showAssaultData &&
-              this.state.assaultData.map(incident => (
-                <AssaultComponent
-                  lat={incident.lat}
-                  lng={incident.long}
-                  text="Assault"
-                />
-              ))}
+              this.state.assaultData.map((incident, index) => {
+                while (index < this.state.shownLimit) {
+                  return (
+                    <AssaultComponent
+                      lat={incident.lat}
+                      lng={incident.long}
+                      text="Assault"
+                    />
+                  );
+                }
+              })}
             {this.state.showTheftData &&
-              this.state.theftData.map(incident => (
-                <TheftComponent
-                  lat={incident.lat}
-                  lng={incident.long}
-                  text="Theft"
-                />
-              ))}
+              this.state.theftData.map((incident, index) => {
+                while (index < this.state.shownLimit) {
+                  return (
+                    <TheftComponent
+                      lat={incident.lat}
+                      lng={incident.long}
+                      text="Theft"
+                    />
+                  );
+                }
+              })}
             {this.state.showCollisionData &&
-              this.state.collisionData.map(incident => (
-                <CollisionComponent
-                  lat={incident.latitude}
-                  lng={incident.longitude}
-                  text="Collision"
-                />
-              ))}
+              this.state.collisionData.map((incident, index) => {
+                while (index < this.state.shownLimit) {
+                  return (
+                    <CollisionComponent
+                      lat={incident.latitude}
+                      lng={incident.longitude}
+                      text="Collision"
+                    />
+                  );
+                }
+              })}
           </GoogleMapReact>
         </div>
       </div>
