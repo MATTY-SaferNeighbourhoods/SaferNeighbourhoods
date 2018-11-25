c21c5035db181ed1ce240bb1605c99bd66ec31cd
TrikaMit <trikamitra@gmail.com>
2018-11-25 13:01:59 -0500

    minor styling changes

--- humz/src/pages/HomePage/HomePage.js
+++ humz/src/pages/HomePage/HomePage.js
@@ -9,15 +9,15 @@ class HomePage extends Component {
         <Header headerName="MATTY Community"/>
         <div class="HomePage__mainbody">
         <AssetsandISO 
-        name="Assets"
-        srcOne='https://i.pinimg.com/originals/8e/bb/c4/8ebbc4fcd99fb30c77ac1a550d09840f.jpg'
-        srcTwo='https://www.westislandblog.com/wp-content/uploads/2017/06/moving-day-tips.jpg'
-        srcThree= 'https://4.imimg.com/data4/EF/EM/MY-12196142/cloth-steamer-500x500.jpg'/>
-        <AssetsandISO 
-        name="ISO"
+        name="What my community is sharing"
         srcOne='https://az616578.vo.msecnd.net/files/2016/04/04/635953734737456159-1660065123_keep-calm-tutor.png'
         srcTwo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGLy-pz6lBxidu8tM_I58hvlOx-GvHaGM2goGNnfoHxEXLPzFn'
         srcThree= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS1cV2aS-qNt7KX0m4lwKkC5hF7xdmLHyDkTCHsz2AiQsc-oFn' />
+        <AssetsandISO 
+        name="What my community needs"
+        srcOne='https://i.pinimg.com/originals/8e/bb/c4/8ebbc4fcd99fb30c77ac1a550d09840f.jpg'
+        srcTwo='https://www.westislandblog.com/wp-content/uploads/2017/06/moving-day-tips.jpg'
+        srcThree= 'https://4.imimg.com/data4/EF/EM/MY-12196142/cloth-steamer-500x500.jpg'/>
 
         </div>
       </div>
